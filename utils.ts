0d076e9dd37be3772dd10e94de6609361a7c8a8a
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2023-10-09 09:39:32 +0100

    updated utils to point to developemnt

--- utils.ts
+++ utils.ts
@@ -1,5 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
 export const serverUrl = dev
-  ? "http://localhost:3000"
-  : "https://your_deployment.server.com";
+  ? "http://localhost:3002"
+  : "https://coderjobs.netlify.app/";
